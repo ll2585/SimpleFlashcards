496435a09ce13c944ece3a2ce91dd810389cfad1
Luke Li <luke.li@nera.com>
2015-04-22 18:52:01 -0400

    Initial commit

---
+++ tests/CardTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CardTest {
+    @Test
+    public void hasFrontAndBack() {
+        Card c = new Card("front", "back");
+        Assert.assertTrue(c.getFront() == "front");
+        Assert.assertTrue(c.getBack() == "back");
+    }
+}
